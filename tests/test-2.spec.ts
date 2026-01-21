e59b3143de834e2cbc9fe07fb0bbb4053d2e076b
najmialazhar <najmishfwn@gmail.com>
2026-01-21 11:22:55 +0700

    Re-Testing TC-UI IDMT

--- tests/test-2.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://10.62.180.28/idmtdev/public/idmt');
-  await page.getByRole('link', { name: 'Dokumen ' }).click();
-  await page.getByRole('link', { name: 'Kelola Dokumen' }).click();
-  await expect(page.getByRole('heading')).toContainText('Kelola Dokumen');
-});
